816ecf4364dcb362a80c90e6d78ec5432ba12fea
Isaak Velasquez <43457877+isaako-mode@users.noreply.github.com>
2020-05-26 03:06:50 -0600

    Update BotSource.py

--- BotSource.py
+++ BotSource.py
@@ -2,12 +2,16 @@ import os
 
 import time
 import discord
-token = "MjgzODczMjc2NDAzNjQ2NDY1.XszCaw.lQTqH0uib37Rt4gAELp-7chLgmo"
+def getToken():            #get token from text file
+    with open('token.txt') as f:
+        return f.read()
+
+token = getToken()      # token var
 
 client = discord.Client()
 
 @client.event
-async def on_message(message):
+async def on_message(message):   #function for responding to messages     
     if message.content.find("hey clinton") != -1:
         time.sleep(1)
         #await client.channel.send("woooooo") #***later version of send message code for testing
@@ -19,4 +23,3 @@ async def on_message(message):
     elif message.content.find("how are you") != -1:
         await client.send_message(message.channel, content="Who said that i have had sexual relations with this server?")
 client.run(token)
-
