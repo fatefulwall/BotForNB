72d83e79fc0795fe9774a18ee4f9d86f673e19e1
Isaak Velasquez <43457877+isaako-mode@users.noreply.github.com>
2020-05-27 13:57:06 -0600

    Update BotSource.py

--- BotSource.py
+++ BotSource.py
@@ -2,24 +2,46 @@ import os
 
 import time
 import discord
-def getToken():            #get token from text file
+import asyncio
+
+def getToken():     # reads function that is stored in text file
     with open('token.txt') as f:
         return f.read()
 
-token = getToken()      # token var
+token = getToken()  # token var
 
 client = discord.Client()
 
+
+
 @client.event
-async def on_message(message):   #function for responding to messages     
-    if message.content.find("hey clinton") != -1:
+async def on_message(message):       # scans for messages
+
+    if message.content.find("show me the history") != -1:
+        l = open('logs.txt', 'w')
+
+        messages = await message.channel.history().flatten()
+        for message in messages:
+            logsource = str(message)
+            l.write(logsource)
+            l.write('\n')
+
+        l.close()
+
+    elif message.content.find("hey clinton") != -1:
         time.sleep(1)
-        #await client.channel.send("woooooo") #***later version of send message code for testing
-        await client.send_message(message.channel, content="Yes, i did have sexual relations with this server")
+        await message.channel.send("Yes, i did have sexual relations with this server")
+        #await client.send_message(message.channel, content="Yes, i did have sexual relations with this server")
 
     elif message.content.find("where are you") != -1:
-        await client.send_message(message.channel, content="https://media3.s-nbcnews.com/i/newscms/2017_28/1228821/bill-clinton-peek-a-boo-today-tease-170714_63755d2cf6a6875559ea2ef9d86d09ff.jpg")
+        time.sleep(1)
+        await message.channel.send("https://media3.s-nbcnews.com/i/newscms/2017_28/1228821/bill-clinton-peek-a-boo-today-tease-170714_63755d2cf6a6875559ea2ef9d86d09ff.jpg")
 
     elif message.content.find("how are you") != -1:
-        await client.send_message(message.channel, content="Who said that i have had sexual relations with this server?")
+        time.sleep(1)
+        await message.channel.send("Who said that i have sexual relations with this server??")
+
+
+
 client.run(token)
+
